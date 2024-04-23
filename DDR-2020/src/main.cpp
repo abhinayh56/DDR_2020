dfdd72012e527b674d6630e5db76b7beabdc9652
abhinayh56 <kumarabhinay53@gmail.com>
2024-04-23 21:43:44 +0530

    temp commit

--- DDR-2020/src/main.cpp
+++ DDR-2020/src/main.cpp
@@ -7,13 +7,8 @@ int loop_timer = (float)dt*1000000;
 float timer = 0;
 
 void initialize_interrupt();
-void run_estimators();
-void estimate_motors_rpm();
-void estimate_vehicle_position();
-void estimate_vehicle_twist();
 void command_motor1(float pwm1);
 void command_motor2(float pwm2);
-float rad2deg(float _rad);
 
 #define A1 18
 #define B1 19
@@ -23,41 +18,9 @@ float rad2deg(float _rad);
 volatile long count1 = 0;
 volatile long count2 = 0;
 
-float th1 = 0;
-float th_pre1 = 0;
-float rpm1 = 0;
-
-float th2 = 0;
-float th_pre2 = 0;
-float rpm2 = 0;
-
-volatile float Dr = 0;
-volatile float Dl = 0;
-volatile float Dc = 0;
-volatile float phi = 0;
-volatile float x = 0;
-volatile float y = 0;
-const float var = 0.000157079632679489661923132169164; //(2.0*PI/1700.0)*(85.0/2.0)*0.001;
-const float L = 0.2;
-const float r = 0.085*0.5;
-const float dphi = 0.00078539816339744830961566084582;
-volatile float dphi_temp = 0;
-const float pi = 3.1415926535897932384626433832795;
-const float pi_2 = 6.283185307179586476925286766559;
-
-float X;
-float Y;
-float PHI;
-
-// linear velocity and angular velocity
-float Vc = 0;
-float Wc = 0;
-
-// deseired twist
-float Vc0 = 0;
-float Wc0 = 0;
-
 void setup() {
+  Serial.begin(9600);
+
   pinMode(4,OUTPUT);
   pinMode(5,OUTPUT);
   pinMode(6,OUTPUT);
@@ -70,12 +33,7 @@ void setup() {
 }
 
 void loop() {
-  Serial.begin(9600);
-  run_estimators();
-
-  command_motor1(0);
-  command_motor2(0);
-  delay(50);
+  delay(10);
 }
 
 
@@ -88,55 +46,26 @@ void initialize_interrupt()
   sei();
 }
 
-ISR(INT2_vect)
-{
+ISR(INT2_vect){
   int a1 = digitalRead(A1);
   int b1 = digitalRead(B1);
   
-  if(a1==1)
-  {
-    if(b1==1)
-    {
+  if(a1==1){
+    if(b1==1){
       count1++;
-      Dr = var;
-      dphi_temp = dphi;
     }
-    else
-    {
+    else{
       count1--;
-      Dr = -var;
-      dphi_temp = -dphi;
     }
   }
-  else
-  {
-    if(b1==0)
-    {
+  else{
+    if(b1==0){
       count1++;
-      Dr = var;
-      dphi_temp = dphi;
     }
-    else
-    {
+    else{
       count1--;
-      Dr = -var;
-      dphi_temp = -dphi;
     }
   }
-
-  //phi += Dr/L;
-  phi += dphi_temp;
-  if(phi>pi)
-  {
-    phi = phi-pi_2;
-  }
-  else if (phi<=-pi)
-  {
-    phi = phi+pi_2;
-  }
-  Dc = Dr*0.5;
-  x += Dc*cos(phi);
-  y += Dc*sin(phi);
 }
 
 ISR(INT3_vect)
@@ -144,201 +73,66 @@ ISR(INT3_vect)
   int a1 = digitalRead(A1);
   int b1 = digitalRead(B1);
   
-  if(b1==1)
-  {
-    if(a1==0)
-    {
+  if(b1==1){
+    if(a1==0){
       count1++;
-      Dr = var;
-      dphi_temp = dphi;
     }
-    else
-    {
+    else{
       count1--;
-      Dr = -var;
-      dphi_temp = -dphi;
     }
   }
-  else
-  {
-    if(a1==1)
-    {
+  else{
+    if(a1==1){
       count1++;
-      Dr = var;
-      dphi_temp = dphi;
     }
-    else
-    {
+    else{
       count1--;
-      Dr = -var;
-      dphi_temp = -dphi;
     }
   }
-
-  //phi += Dr/L;
-  phi += dphi_temp;
-  if(phi>pi)
-  {
-    phi = phi-pi_2;
-  }
-  else if (phi<=-pi)
-  {
-    phi = phi+pi_2;
-  }
-  Dc = Dr*0.5;
-  x += Dc*cos(phi);
-  y += Dc*sin(phi);
 }
 
-ISR(INT4_vect)
-{
+ISR(INT4_vect){
   int a2 = digitalRead(A2);
   int b2 = digitalRead(B2);
   
-  if(a2==1)
-  {
-    if(b2==1)
-    {
+  if(a2==1){
+    if(b2==1){
       count2++;
-      Dl = var;
-      dphi_temp = -dphi;
     }
-    else
-    {
+    else{
       count2--;
-      Dl = -var;
-      dphi_temp = dphi;
     }
   }
-  else
-  {
-    if(b2==0)
-    {
+  else{
+    if(b2==0){
       count2++;
-      Dl = var;
-      dphi_temp = -dphi;
     }
-    else
-    {
+    else{
       count2--;
-      Dl = -var;
-      dphi_temp = dphi;
     }
   }
-
-  //phi -= Dl/L;
-  phi += dphi_temp;
-  if(phi>pi)
-  {
-    phi = phi-pi_2;
-  }
-  else if (phi<=-pi)
-  {
-    phi = phi+pi_2;
-  }
-  Dc = Dl*0.5;
-  x += Dc*cos(phi);
-  y += Dc*sin(phi);
 }
 
-ISR(INT5_vect)
-{
+ISR(INT5_vect){
   int a2 = digitalRead(A2);
   int b2 = digitalRead(B2);
   
-  if(b2==1)
-  {
-    if(a2==0)
-    {
+  if(b2==1){
+    if(a2==0){
       count2++;
-      Dl = var;
-      dphi_temp = -dphi;
     }
-    else
-    {
+    else{
       count2--;
-      Dl = -var;
-      dphi_temp = dphi;
     }
   }
-  else
-  {
-    if(a2==1)
-    {
+  else{
+    if(a2==1){
       count2++;
-      Dl = var;
-      dphi_temp = -dphi;
     }
-    else
-    {
+    else{
       count2--;
-      Dl = -var;
-      dphi_temp = dphi;
     }
   }
-
-  //phi -= Dl/L;
-  phi += dphi_temp;
-  if(phi>pi)
-  {
-    phi = phi-pi_2;
-  }
-  else if (phi<=-pi)
-  {
-    phi = phi+pi_2;
-  }
-  Dc = Dl*0.5;
-  x += Dc*cos(phi);
-  y += Dc*sin(phi);
-}
-
-void run_estimators()
-{
-  estimate_motors_rpm();
-  estimate_vehicle_position();
-  estimate_vehicle_twist();
-}
-
-void estimate_motors_rpm()
-{
-  // angle of rotation of motor
-  th1 = (360.0/1700.0)*(float)count1;
-  th2 = (360.0/1700.0)*(float)count2;
-  // Serial.print(th1); Serial.print('\t');
-  // Serial.println(th2);
-
-  // rpm of motor
-  rpm1 = (th1 - th_pre1)/dt;
-  th_pre1 = th1;
-  rpm2 = (th2 - th_pre2)/dt;
-  th_pre2 = th2;
-  Serial.print(rpm1); Serial.print('\t');
-  Serial.println(rpm2);
-}
-
-void estimate_vehicle_position()
-{
-  X = x*100.0;
-  Y = y*100.0;
-  PHI = rad2deg(phi);
-
-  //Serial.print(X); Serial.print('\t');
-  //Serial.print(Y); Serial.print('\t');
-  //Serial.println(PHI);
-}
-
-void estimate_vehicle_twist()
-{
-  Vc = 0.000370882466048795*(rpm1+rpm2);
-  Wc = 0.2125*(rpm1-rpm2);
-  //Serial.print(Vc); Serial.print('\t');
-  //Serial.println(Wc);
-}
-
-float rad2deg(float _rad)
-{
-  //return _rad*180/PI;
-  return _rad*57.295779513082320876798154814105;
 }
 
 void command_motor1(float pwm1)
