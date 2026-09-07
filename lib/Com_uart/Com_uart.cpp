dca3571914f482281b88f4e44e77dc278d70bebe
abhinayh56 <kumarabhinay53@gmail.com>
2026-09-07 17:05:56 +0530

    asdfasdf

--- lib/Com_uart/Com_uart.cpp
+++ lib/Com_uart/Com_uart.cpp
@@ -6,74 +6,75 @@ Com_uart::Com_uart()
 
 void Com_uart::init(HardwareSerial &serial, unsigned long baud, double timeout_rx_s, double main_loop_freq, double com_uart_tx_freq)
 {
-    _serial = &serial;
-    _serial->begin(baud);
-    tx_pkt.drive_mode = 0x00;
-    tx_pkt.id = 0x05;
-    com_uart_tx_counter_max = (unsigned long)(main_loop_freq / com_uart_tx_freq);
-    last_t_rx_us = micros();
-    timeout_rx_us = (unsigned long)(timeout_rx_s * 1000000.0);
+	_serial = &serial;
+	_serial->begin(baud);
+	tx_pkt.drive_mode = 0x00;
+	tx_pkt.id = 0x05;
+	com_uart_tx_counter_max = (unsigned long)(main_loop_freq / com_uart_tx_freq);
+	last_t_rx_us = micros();
+	timeout_rx_us = (unsigned long)(timeout_rx_s * 1000000.0);
 }
 
 void Com_uart::com_rx(uint8_t &drive_mode, double &cmd_1, double &cmd_2)
 {
-    // start_byte, drive_mode, data_1, data_2, end_byte
-    while (_serial->available())
-    {
-        uint8_t data = _serial->read();
-        if (data == 0x21)
-        {
-            rx_buff_index = 0;
-        }
-        rx_buff[rx_buff_index] = data;
-        rx_buff_index++;
+	// start_byte, drive_mode, data_1, data_2, end_byte
+	while (_serial->available())
+	{
+		uint8_t data = _serial->read();
+		if (data == 0x21)
+		{
+			rx_buff_index = 0;
+		}
+		rx_buff[rx_buff_index] = data;
+		rx_buff_index++;
 
-        if (rx_buff_index > 10)
-        {
-            rx_buff_index = 10;
-        }
-        if (data == 0x23)
-        {
-            if (rx_buff[0] == 0x21)
-            {
-                memcpy(&rx_pkt, rx_buff, 11);
-                last_t_rx_us = micros();
-            }
-        }
-    }
+		if (rx_buff_index > 10)
+		{
+			rx_buff_index = 10;
+		}
+		if (data == 0x23)
+		{
+			if (rx_buff[0] == 0x21)
+			{
+				memcpy(&rx_pkt, rx_buff, 11);
+				last_t_rx_us = micros();
+			}
+		}
+	}
 
-    if ((micros() - last_t_rx_us) > timeout_rx_us)
-    {
-        drive_mode = 0;
-        cmd_1 = 0;
-        cmd_2 = 0;
-    }
-    else
-    {
-        drive_mode = rx_pkt.drive_mode;
-        cmd_1 = rx_pkt.cmd_1;
-        cmd_2 = rx_pkt.cmd_2;
-    }
+	if ((micros() - last_t_rx_us) > timeout_rx_us)
+	{
+		drive_mode = 0;
+		cmd_1 = 0;
+		cmd_2 = 0;
+	}
+	else
+	{
+		drive_mode = rx_pkt.drive_mode;
+		cmd_1 = rx_pkt.cmd_1;
+		cmd_2 = rx_pkt.cmd_2;
+	}
 }
 
 void Com_uart::com_tx(const uint8_t drive_mode, const double x, const double y, const double th, const double v, const double w)
 {
-    if (com_uart_tx_counter == com_uart_tx_counter_max)
-    {
-        tx_pkt.t_millis = millis();
-        tx_pkt.drive_mode = drive_mode;
-        tx_pkt.x = x;
-        tx_pkt.y = y;
-        tx_pkt.th = th;
-        tx_pkt.v = v;
-        tx_pkt.w = w;
-        memcpy(tx_buff, &tx_pkt, 28);
-        _serial->write(tx_buff, 28);
-    }
+	if (com_uart_tx_counter == com_uart_tx_counter_max)
+	{
+		tx_pkt.t_millis = millis();
+		tx_pkt.drive_mode = drive_mode;
+		tx_pkt.x = x;
+		tx_pkt.y = y;
+		tx_pkt.th = th;
+		tx_pkt.v = v;
+		tx_pkt.w = w;
+		memcpy(tx_buff, &tx_pkt, 28);
+		_serial->write(tx_buff, 28);
+	}
 
-    com_uart_tx_counter++;
-    if (com_uart_tx_counter > com_uart_tx_counter_max)
-    {
-        com_uart_tx_counter = 1;
-    }
+	com_uart_tx_counter++;
+
+	if (com_uart_tx_counter > com_uart_tx_counter_max)
+	{
+		com_uart_tx_counter = 1;
+	}
 }
